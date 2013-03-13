65f0b91cb037128d9308f9ca4063235a1479dc3f
alexmarkup <alexandrerivest@gmail.com>
2013-03-13 17:02:47 -0400

    static markup

--- index.php
+++ index.php
@@ -7,12 +7,26 @@
 	
 	<body>
 		
-		<?php
-			$change;
-			$deuxieeTest;
-			$test = "aa";
+		<div>
+			<header>
+				
+			</header>
+			<div>
+				<figure>
+					<figcaption>Vue du modèle de chandail</figcaption>
+					<img src="img_pulpit.jpg" alt="The Pulpit Rock" width="304" height="228">
+				</figure>
+				<article>
+					<h1>Sélection de la couleur</h1>
+					
+				</article>
+			</div>
+			<?php
+				$change;
+				$deuxieeTest;
+				$test = "aa";
 		
-		?>
-		
+				?>
+		</div>
 	</body>
 </html>
