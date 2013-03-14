8cede58572050c2d876ce002bf64fd6536447bc1
alexmarkup <alexandrerivest@gmail.com>
2013-03-13 20:15:13 -0400

    last commit

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 	<head>
 		<meta charset="UTF-8">
 		<title>untitled</title>
+		<link rel="stylesheet" type="text/css" href="css/main.css">
+		<script type="text/javascript" src="scripts/main.js"></script>
 	</head>
 	
 	<body>
@@ -14,11 +16,18 @@
 			<div>
 				<figure>
 					<figcaption>Vue du modèle de chandail</figcaption>
-					<img src="img_pulpit.jpg" alt="The Pulpit Rock" width="304" height="228">
+					<!-- img src="img_pulpit.jpg" alt="The Pulpit Rock" width="304" height="228" -->
 				</figure>
 				<article>
 					<h1>Sélection de la couleur</h1>
-					
+					<form>
+						<fieldset>
+							<legend>Colors</legend>
+							<!-- input type="text" id="color" name="color" value="#123456" / -->
+							<div id="color" style="width: 200px; height: 200px;"></div>
+						</fieldset>
+					</form>
+					 <div id="colorpicker"></div>
 				</article>
 			</div>
 			<?php
